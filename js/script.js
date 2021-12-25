d4cfc3bbd075f1b1b0a4cf4cf5e0669bbb9856fb
Ivanna_1109 <ivanna.kovaliv1215@gmail.com>
2021-12-25 14:58:23 +0300

    commit1

--- js/script.js
+++ js/script.js
@@ -38,3 +38,75 @@ function getRadius () {
     alert (`Your result ${result} `);
 
 }
+
+function getSpeed () {
+    let distanse;
+    let time;
+    distanse = +prompt ('Input your distanse');
+    time = +prompt ('Input your time');
+    let speed;
+    speed = distanse/time;
+if (distanse<0 || time <0) {
+    alert ('Error')
+} else if (distanse===0 || time===0) {
+    alert ('Good luck!')
+}else {
+    alert (`Your speed ${speed}`);
+}
+}
+
+function getCurrencyConverter() {
+    let currency;
+    const exchangeRate = 0.88 ;
+    currency  = +prompt ('Input the amount in dollars');
+    let euro = currency * exchangeRate;
+    if (currency < 0) {
+        alert ('Error');
+    } else if ( currency===0) {
+        alert ('Good luck')
+    } else {
+        alert (`Your amount ${euro} Euro`);
+    }
+}
+
+function getNumberOfFiles () {
+    let volume;
+    volume = +prompt ('Input the volume of the flash drive');
+    const size = 0.820 ;
+    let num = Math.floor ( volume / size ); 
+    if (volume < 0 ) {
+        alert ('Error')
+    } else if (volume===0) {
+        alert ('Good luck');
+    } else {
+        alert (`Your number of files ${num} `);
+    }
+}
+
+function getNumChocolate () {
+    let amount = +prompt ('Input the amount of money in your wallet');
+    let price = +prompt ('Input price chocolate');
+    let num = Math.floor (amount/price);
+    let costs = num * price;
+    let balance = amount-costs;
+    if ( amount<0||amount ===0) {
+        alert ('You have no money')
+    } else if (price === 0 || price < 0) {
+        alert ('no price');
+    } else {
+        alert (`You can buy ${num} chocolate, your balance ${balance}`)
+    }
+
+}
+
+function getAmountInterest () {
+    let amount;
+    amount = +prompt ('Input your amount');
+    const interest = 0.00833
+    let result = amount*interest;
+if (amount<=0 ) {
+    alert ('Good luck')
+} else {
+    alert (`Your amount interest ${result}`)
+}
+}
