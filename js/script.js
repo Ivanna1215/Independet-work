573a0749696cbe785f1b50133a730cfa46e48814
Ivanna_1109 <ivanna.kovaliv1215@gmail.com>
2021-12-28 13:13:13 +0300

    commit2812

--- js/script.js
+++ js/script.js
@@ -144,3 +144,20 @@ for (let j = 1; j<=10; j++) {
 console.log ('')
     }
 }
+
+function WhoAreYou () {
+    let age;
+    age = +prompt('How old are you?');
+    if (age === 0 && age<=12 ) {
+alert ('You are a child')
+    } else if (age>=12 && age<=18) {
+        alert ('You are a teenager') 
+    } else if (age>=18 && age<60) {
+        alert ('You are an adult') 
+    } else if (age>=60) {
+        alert ('You are  retired')
+    } else if (age < 0) {
+        alert ('Your age is wrong ')
+    }
+
+}
